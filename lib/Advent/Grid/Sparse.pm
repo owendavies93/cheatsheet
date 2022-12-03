6cd3c652185cf08b8e2ba486ba09dcedc913b26e
Owen Davies <owen@odavi.es>
2022-12-03 19:08:32 +0000

    Add sparse grid implementation

---
+++ lib/Advent/Grid/Sparse.pm
@@ -0,0 +1,57 @@
+package Advent::Grid::Sparse;
+
+use Mojo::Base 'Advent::Grid';
+
+sub new {
+    my $class = shift;
+    my $self = {
+        grid => {},
+    };
+    bless $self, $class;
+    return $self;
+}
+
+# return the total number of coordinates with values
+sub count_set {
+    my $self = shift;
+    return scalar keys %{$self->{grid}};
+}
+
+# return value at an (x, y) coordinate
+sub get {
+    my ($self, $x, $y) = @_;
+    return $self->{grid}->{$x, $y};
+}
+
+# incrememnt the value at (x, y)
+sub inc {
+    my ($self, $x, $y) = @_;
+    $self->{grid}->{$x, $y}++;
+}
+
+# return values of neighbours given an x, y coordinate
+sub neighbour_values {
+    my ($self, $x, $y) = @_;
+    return map { $self->get($_->[0], $_->[1]) } $self->neighbours($x, $y);
+}
+
+# return neighbour coordinates given an x, y coordinate
+sub neighbours {
+    my ($self, $x, $y) = @_;
+
+    my @ns = ();
+    for my $dy (-1..1) {
+        for my $dx (-1..1) {
+            push @ns, [$x + $dx, $y + $dy];
+        }
+    }
+    return @ns;
+}
+
+# sets the value of the given x, y coordinate
+sub set {
+    my ($self, $x, $y, $v) = @_;
+    $self->{grid}->{$x, $y} = $v;
+}
+
+1;
