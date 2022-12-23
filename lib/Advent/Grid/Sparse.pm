6b2e567977ab6dd87c723ba5e931639eaa2294f3
Owen Davies <owen@odavi.es>
2022-12-23 10:11:29 +0000

    Add some more useful methods to Grid::Sparse

--- lib/Advent/Grid/Sparse.pm
+++ lib/Advent/Grid/Sparse.pm
@@ -11,10 +11,22 @@ sub new {
     return $self;
 }
 
+# return all set points in the grid (no values)
+sub all {
+    my $self = shift;
+    return keys %{$self->{grid}};
+}
+
 # return the total number of coordinates with values
 sub count_set {
     my $self = shift;
     return scalar keys %{$self->{grid}};
+}
+
+# remove a point from the grid
+sub delete {
+    my ($self, $x, $y) = @_;
+    delete $self->{grid}->{$x, $y}; 
 }
 
 # return value at an (x, y) coordinate
@@ -47,6 +59,31 @@ sub neighbours {
         }
     }
     return @ns;
+}
+
+# print the grid in the AOC style, using the bounding box of the point set
+sub print {
+    my $self = shift;
+	my $minx = my $miny = ~0;
+  	my $maxx = my $maxy = 0;
+     
+  	for my $p ($self->all()) {
+		my ($x, $y) = split $;, $p;
+		$minx = $x if $x < $minx;
+		$maxx = $x if $x > $maxx;
+		$miny = $y if $y < $miny;
+		$maxy = $y if $y > $maxy;
+  	}
+
+	print "\n";
+	for my $y ($miny..$maxy) {
+		for my $x ($minx..$maxx) {
+			my $v = $self->get($x, $y);
+			print defined $self->get($x, $y) ? $v : '.';
+		}
+		print "\n";
+	}
+	print "\n";
 }
 
 # sets the value of the given x, y coordinate
