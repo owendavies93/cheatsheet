07656e9d76b26755fd51a435082d9a0345bf0681
Owen Davies <owen@odavi.es>
2022-12-27 18:15:32 +0000

    Use point class in Sparse grid
    This is quite a lot slower so perhaps isn't always ideal, depends
    on the task probably.

--- lib/Advent/Grid/Sparse.pm
+++ lib/Advent/Grid/Sparse.pm
@@ -2,6 +2,8 @@ package Advent::Grid::Sparse;
 
 use Mojo::Base 'Advent::Grid';
 
+use Advent::Point::Point2;
+
 sub new {
     my $class = shift;
     my $self = {
@@ -25,71 +27,76 @@ sub count_set {
 
 # remove a point from the grid
 sub delete {
-    my ($self, $x, $y) = @_;
-    delete $self->{grid}->{$x, $y}; 
+    my ($self, $p) = @_;
+    delete $self->{grid}->{$p->key}; 
 }
 
 # return value at an (x, y) coordinate
 sub get {
-    my ($self, $x, $y) = @_;
-    return $self->{grid}->{$x, $y};
+    my ($self, $p) = @_;
+    return $self->{grid}->{$p->key};
 }
 
 # incrememnt the value at (x, y)
 sub inc {
-    my ($self, $x, $y) = @_;
-    $self->{grid}->{$x, $y}++;
+    my ($self, $p) = @_;
+    $self->{grid}->{$p}++;
 }
 
 # return values of neighbours given an x, y coordinate
 sub neighbour_values {
-    my ($self, $x, $y) = @_;
-    return map { $self->get($_->[0], $_->[1]) } $self->neighbours($x, $y);
+    my ($self, $p) = @_;
+    return map { $self->get($_) } $self->neighbours($p);
 }
 
 # return neighbour coordinates given an x, y coordinate
 sub neighbours {
-    my ($self, $x, $y) = @_;
+    my ($self, $p) = @_;
 
     my @ns = ();
     for my $dy (-1..1) {
         for my $dx (-1..1) {
             next if $dx == 0 and $dy == 0;
-            push @ns, [$x + $dx, $y + $dy];
+            my $p2 = Advent::Point::Point2->new($dx, $dy);
+            my $p3 = $p->add($p2);
+            push @ns, $p3;
         }
     }
     return @ns;
 }
 
 # print the grid in the AOC style, using the bounding box of the point set
+# poor memory usage, creates points twice
 sub print {
     my $self = shift;
-	my $minx = my $miny = ~0;
-  	my $maxx = my $maxy = 0;
+    my $minx = my $miny = ~0;
+    my $maxx = my $maxy = 0;
      
-  	for my $p ($self->all()) {
-		my ($x, $y) = split $;, $p;
-		$minx = $x if $x < $minx;
-		$maxx = $x if $x > $maxx;
-		$miny = $y if $y < $miny;
-		$maxy = $y if $y > $maxy;
-  	}
+    for my $i ($self->all()) {
+        my ($x, $y) = split $;, $i;
+        my $p = Advent::Point::Point2->new($x, $y);
+        $minx = $p->x if $p->x < $minx;
+        $maxx = $p->x if $p->x > $maxx;
+        $miny = $p->y if $p->y < $miny;
+        $maxy = $p->y if $p->y > $maxy;
+    }
 
-	print "\n";
-	for my $y ($miny..$maxy) {
-		for my $x ($minx..$maxx) {
-			my $v = $self->get($x, $y);
-			print defined $self->get($x, $y) ? $v : '.';
-		}
-		print "\n";
-	}
-	print "\n";
+    print "\n";
+    for my $y ($miny..$maxy) {
+        for my $x ($minx..$maxx) {
+            my $p = Advent::Point::Point2->new($x, $y);
+            my $v = $self->get($p);
+            print ($v // '.');
+        }
+        print "\n";
+    }
+    print "\n";
 }
 
 # sets the value of the given x, y coordinate
 sub set {
-    my ($self, $x, $y, $v) = @_;
-    $self->{grid}->{$x, $y} = $v;
+    my ($self, $p, $v) = @_;
+    $self->{grid}->{$p->key} = $v;
 }
 
 1;
