926fb600696b70a89d84f5629be995a92ceb2c34
Owen Davies <owen@odavi.es>
2023-12-02 13:25:54 +0000

    Fix indentation; add submit to Problem utils

--- lib/Advent/Utils/Problem.pm
+++ lib/Advent/Utils/Problem.pm
@@ -2,84 +2,104 @@ package Advent::Utils::Problem;
 
 use Mojo::Base 'Exporter';
 
+use Cwd;
 use FindBin;
 use Term::ANSIColor qw(:constants colored);
 use Time::HiRes qw(time);
 
 our @EXPORT_OK = qw(
-	run_part
-	submit_part
+    run_part
+    submit
+    submit_part
 );
 
 sub _display_aoc_response {
-	my $res = shift;
+    my $res = shift;
 
-  	my $known_responses = {
-      	"You don't seem"     => 'red',
-      	"That's the correct" => 'green',
-    	"That's not"         => 'red',
-  	};
+    $res =~ s/\R//g;
 
-	my $printed = 0;
-  	for my $c (keys %$known_responses) {
-		if ($res =~ $c) {
-	  		say colored($res, $known_responses->{$c});
-			$printed = 1;
-		  	last;
-		}
+      my $known_responses = {
+          "You don't seem"     => 'red',
+          "That's the correct" => 'green',
+          "That's not"         => 'red',
+      };
+
+    my $printed = 0;
+    for my $c (keys %$known_responses) {
+        if ($res =~ $c) {
+            say colored($res, $known_responses->{$c});
+            $printed = 1;
+            last;
+        }
     }
-	say $res if $printed == 0;
+    say $res if $printed == 0;
 }
 
 sub _find_year {
-	my ($path, $dir) = @_;
+    my ($path, $dir) = @_;
 
-  	if (-d "$path/$dir") {
-    	return "$path/$dir";
-  	} else {
-    	return _find_year("$path/..", $dir);
-  	}
+    if (-d "$path/$dir") {
+        return "$path/$dir";
+    } else {
+        return _find_year("$path/..", $dir);
+    }
 }
 
 sub run_part {
-	my ($year, $day, $part) = @_; 
+    my ($year, $day, $part) = @_;
 
-	my $path = _find_year($FindBin::Bin, "advent$year");
+    my $path = _find_year($FindBin::Bin, "advent$year");
 
-	my $t = time;
-	my $out = `perl $path/bin/day$day-$part.pl`;
-	my $t2 = time;
-	print WHITE "Part $part answer: ";
-	print GREEN $out;
+    my $t = time;
+    my $out = `perl $path/bin/day$day-$part.pl`;
+    my $t2 = time;
+    print WHITE "Part $part answer: ";
+    print GREEN $out;
 
-	my $diff = int(($t2 - $t) * 1000);
-	print WHITE 'Runtime: ';
-	print GREEN "$diff ms\n";
+    my $diff = int(($t2 - $t) * 1000);
+    print WHITE 'Runtime: ';
+    print GREEN "$diff ms\n";
 
-	return $out;
+    return $out;
+}
+
+# Assumes standard filename formats and dir structures
+# Only use this for submitting the problem currently
+# being worked on
+sub submit {
+    my $answer = shift;
+    my ($year) = cwd =~ /advent(\d+)$/;
+    my ($day, $part) = $0 =~ /day(\d+)-(\d)/;
+    say WHITE "Submitting $year day $day part $part";
+    _submit($year, $day, $part, $answer);
+    exit;
 }
 
 sub submit_part {
-	my ($year, $day, $part, $answer) = @_;
-	ASK:
-	say RESET "Do you want to submit this answer? [Y/n]";        
-	my $yes = <STDIN>;
-	chomp $yes;
+    my ($year, $day, $part, $answer) = @_;
+    ASK:
+    say RESET "Do you want to submit this answer? [Y/n]";
+    my $yes = <STDIN>;
+    chomp $yes;
 
-	if ($yes =~ /^[Nn]$/) {
-		warn "Not submitting part $part.\n";
-	  	return;
-	} elsif ($yes ne '' && $yes !~ /^[Yy]$/) {
-	  	warn "Please provide y or n.\n";
-	  	goto ASK;
-	}
+    if ($yes =~ /^[Nn]$/) {
+        warn "Not submitting part $part.\n";
+        return;
+    } elsif ($yes ne '' && $yes !~ /^[Yy]$/) {
+        warn "Please provide y or n.\n";
+        goto ASK;
+    }
 
-	print WHITE "Submitting ";
-	say GREEN $answer;
+    _submit($year, $day, $part, $answer);
+}
 
-	my $res = `aoc submit -q -d $day $part $answer`;
-	say RESET "Response from AOC:";
-	_display_aoc_response($res);
+sub _submit {
+    my ($year, $day, $part, $answer) = @_;
+    print WHITE "Submitting ";
+    say GREEN $answer;
+    my $res = `aoc submit -q -d $day $part $answer`;
+    say RESET "Response from AOC:";
+    _display_aoc_response($res);
 }
 
 1;
